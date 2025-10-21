203272b52d14ca0ee00c624d96d213b2580f7053
ahitagnied <ahitagnis@gmail.com>
2025-10-21 22:33:11 +0000

    finished publisher

--- src/01-pub-sub/01-pub-sub/talker.py
+++ src/01-pub-sub/01-pub-sub/talker.py
@@ -10,26 +10,23 @@ T = 0.5
 class TalkerNode(Node):
     def __init__(self):
         super().__init__('talker')
-        self.publisher_ = self.create_publisher(String, 'topic', 10) # topic, messgae type string, queue size 10
-        self.timer_ = self.create_timer(T, self.timer_callback) # call timer callback every T seconds
-        self.count = 0
+        self.publisher = self.create_publisher(String, 'topic', 10)
+        self.timer = self.create_timer(T, self.callback)
     
-    def timer_callback(self):
+    def callback(self):
         msg = String()
-        msg.data = 'hello, ros2! %d' % self.count
-        self.count += 1
+        msg.data = 'hello ros2!'
+        self.publisher.publish(msg)
         self.get_logger().info('publishing: "%s"' % msg.data)
-
 
 def main(args=None):
     rclpy.init(args=args)
 
-    # make node
-
-    # use node 
-
-    # destroy node
-
+    node = TalkerNode()
+    
+    rclpy.spin(node)
+    
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
